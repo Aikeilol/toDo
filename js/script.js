71307eda2eb37b389e3b7fcf0570d096914e38aa
Aikeilol <garena3307@mail.ru>
2022-02-10 22:01:58 +0300

    minor changes

--- js/script.js
+++ js/script.js
@@ -1,53 +1,65 @@
-let dragging = false;
-const listItems = document.querySelectorAll('li');
 const body = document.body;
-const liParent = listItems[0].parentNode;
+const liParent = body.querySelector('.parent');
 const dragList = document.getElementById("dragList");
-let draggedItem;
-let hoveredItem;
 
-listItems.forEach(li => {
-  li.addEventListener('mousedown', (event) => {
+enableDragAndDrop()
+
+fetch('https://jsonplaceholder.typicode.com/todos')
+  .then(response => response.json())
+  .then(data => data.map(item => `<li>${item.title}</li>`))
+  .then(liHtmlChuks => {
+    liParent.innerHTML = liHtmlChuks.slice(0, 15).join('');
+    liParent.innerHTML += liHtmlChuks.slice(16, 25).join('');
+  })
+
+
+
+function enableDragAndDrop() {
+  let dragging, draggedItem, hoveredItem;
+
+  liParent.addEventListener('mousedown', (event) => {
+    if (event.target === liParent) return;
     event.preventDefault();
     dragging = true;
-    console.log(event);
     draggedItem = event.target;
     dragList.append(draggedItem);
     body.style.cursor = 'grabbing';
   })
 
-  li.addEventListener('mouseover', () => {
-    if (dragging) hoveredItem = li;
+  liParent.addEventListener('mouseover', (event) => {
+    if (dragging && event.target != liParent) hoveredItem = event.target;
   })
 
-  li.addEventListener('mouseout', (event) => {
+  liParent.addEventListener('mouseout', (event) => {
+    if (event.target === liParent) return;
     if (hoveredItem) hoveredItem.style = ``;
     hoveredItem = null;
   })
-})
 
-body.addEventListener('mouseup', (event) => {
-  dragging = false;
-  if (draggedItem) liParent.insertBefore(draggedItem, hoveredItem);
-  if (hoveredItem) hoveredItem.style = ``;
-  hoveredItem = null;
-  body.style.cursor = null;
-  draggedItem = null;
-})
+  body.addEventListener('mouseup', () => {
+    dragging = false;
+    if (draggedItem) liParent.insertBefore(draggedItem, hoveredItem);
+    if (hoveredItem) hoveredItem.style = ``;
+    hoveredItem = null;
+    body.style.cursor = null;
+    draggedItem = null;
+  })
 
-body.addEventListener('mousemove', (event) => {
-  event.preventDefault();
-  if (!dragging) return;
-  dragList.style.top = `${event.y - 20}px`;
-  dragList.style.left = `${event.x - 30}px`;
-  if (hoveredItem) hoveredItem.style = `padding-top: 17px `;
-})
+  body.addEventListener('mousemove', (event) => {
+    event.preventDefault();
+    if (!dragging) return;
+    dragList.style.top = `${event.y - 20}px`;
+    dragList.style.left = `${event.x - 30}px`;
+    if (hoveredItem) hoveredItem.style = `padding-top: 17px `;
+  })
 
-body.addEventListener('mouseleave', () => {
-  dragging = false;
-  if (draggedItem) liParent.append(draggedItem);
-  if (hoveredItem) hoveredItem.style = ``;
-  hoveredItem = null;
-  body.style.cursor = null;
-  draggedItem = null;
-})
+  body.addEventListener('mouseleave', () => {
+    dragging = false;
+    if (draggedItem) liParent.append(draggedItem);
+    if (hoveredItem) hoveredItem.style = ``;
+    hoveredItem = null;
+    body.style.cursor = null;
+    draggedItem = null;
+  })
+}
+
